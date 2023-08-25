65aab5ff14261cbefe0bd6de961f1a94e30108c7
Orion-zhen <public4orion@163.com>
2023-08-25 15:49:48 +0800

    Fix: spell mistake

--- AIserver.py
+++ AIserver.py
@@ -10,7 +10,7 @@ from transformers.generation import GenerationConfig
 
 
 class AIserver:
-    parser = argparse.AurgumentParser(description="choose a model to load")
+    parser = argparse.ArgumentParser(description="choose a model to load")
     parser.add_argument("--chatglm2", "--chatglm", action="store_true", default=False, help="load chatglm2-6b")
     parser.add_argument("-chatglm2-32k", "--chatglm-32k", action="store_true", default=False, help="load chatglm2-32k")
     parser.add_argument("--qwen", "--Qwen", action="store_true", default=False, help="load 通义千问-7b")
