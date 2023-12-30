fd08be212cf44c56cbbffbb61cd95f6d40f82798
Orion <128988082+Orion-zhen@users.noreply.github.com>
2023-12-30 17:50:14 +0800

    Update: configs compatitable

--- openai.py
+++ openai.py
@@ -1,3 +1,5 @@
+from config.model_config import LLM_ROOT_DIR, LLM_CARD, LLM
+from config.server_config import HOST, PORT
 import subprocess
 import signal
 import time
@@ -17,47 +19,62 @@ def siginit_handler(signum, frame):
     print("Exiting...")
     for p in process_list:
         p.terminate()
-    time.sleep(1)
+    time.sleep(0.5)
     exit(0)
 
 
-host = "0.0.0.0"
-port = 11451
-model_path = "/home/orion/ai/Models/chatglm3-6b"
-model_names = [
-    "gpt-3.5-turbo",
-    "gpt-4",
-    "gpt-4-turbo",
-    "text-davinci-003",
-    "text-embedding-ada-002",
-]
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, siginit_handler)
+    console_msg = (
+        f"| Loading {LLM}"
+        + (
+            f" with extra params {LLM_CARD[LLM]['params']}"
+            if LLM_CARD[LLM]["params"]
+            else ""
+        )
+        + " 🤗| "
+    )
+    
+    print("\n" + "=" * len(console_msg))
+    print(console_msg)
+    print("=" * len(console_msg) + "\n")
+    
+    model_path = os.path.join(LLM_ROOT_DIR, LLM_CARD[LLM]["path"])
+    model_names = [
+        "gpt-3.5-turbo",
+        "gpt-4",
+        "gpt-4-turbo",
+        "text-davinci-003",
+        "text-embedding-ada-002",
+    ]
 
-cmd_server_controller = ["python", "-m", "fastchat.serve.controller"]
-cmd_model_worker = [
-    "python",
-    "-m",
-    "fastchat.serve.model_worker",
-    "--model-names",
-    ",".join(model_names),
-    "--model-path",
-    model_path,
-]
-cmd_openai_api_server = [
-    "python",
-    "-m",
-    "fastchat.serve.openai_api_server",
-    "--host",
-    host,
-    "--port",
-    str(port),
-]
+    cmd_server_controller = ["python", "-m", "fastchat.serve.controller"]
+    cmd_model_worker = [
+        "python",
+        "-m",
+        "fastchat.serve.model_worker",
+        "--model-names",
+        ",".join(model_names),
+        "--model-path",
+        model_path,
+    ]
+    cmd_openai_api_server = [
+        "python",
+        "-m",
+        "fastchat.serve.openai_api_server",
+        "--host",
+        str(HOST),
+        "--port",
+        str(PORT),
+    ]
 
-p_server_controller = subprocess.Popen(cmd_server_controller)
-p_model_worker = subprocess.Popen(cmd_model_worker)
-p_openai_api_server = subprocess.Popen(cmd_openai_api_server)
-process_list = [p_server_controller, p_model_worker, p_openai_api_server]
+    for param in LLM_CARD[LLM]["params"]:
+        cmd_model_worker.append(param)
 
-signal.signal(signal.SIGINT, siginit_handler)
+    p_server_controller = subprocess.Popen(cmd_server_controller)
+    p_model_worker = subprocess.Popen(cmd_model_worker)
+    p_openai_api_server = subprocess.Popen(cmd_openai_api_server)
+    process_list = [p_server_controller, p_model_worker, p_openai_api_server]
 
-for p in process_list:
-    p.wait()
+    for p in process_list:
+        p.wait()
